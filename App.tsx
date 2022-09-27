d38a048d2ef491b92f4b692d212d30f66f22b2cc
Anupama Ellath <anupamae8@gmail.com>
2022-09-27 21:31:40 +0200

    set up theme

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Switch} from 'react-native';
+import { ThemeContext } from './src/components/context/ThemeContext';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [theme, setTheme] = useState("light");
   return (
-    <View style={styles.container}>
-      <Text>Hi am here</Text>
+    <ThemeContext.Provider value={theme}>
+    <View style={theme === 'light'? styles.container :  styles.dark_theme}>
+      <Switch value={theme === 'light'} 
+        onValueChange ={() => setTheme(theme === 'light'? 'dark' : 'light')} />
     </View>
-   
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    minheight: '100vh'
   },
+  dark_theme: {
+    backgroundColor: '#000',
+    alignItems: 'center',
+    minHeight: '100vh'
+  }
 });
